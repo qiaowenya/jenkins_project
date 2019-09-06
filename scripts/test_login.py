c2f5d9e9fee2f74120b8b1167501dc120beb99cc
qiaowenya <1694197678@qq.com>
2019-09-06 10:37:34 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,10 @@
+class TestLogin:
+
+    def test_login_01(self):
+        print('test_login_01')
+
+    def test_login_02(self):
+        print('test_login_01')
+
+    def test_login_03(self):
+        print('test_login_01')
